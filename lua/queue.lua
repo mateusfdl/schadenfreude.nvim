73ae3ffaf9b54b60f0938f061565b40ca7667de7
Matheus Lima <matheus.limastack@gmail.com>
2025-10-13 19:21:47 -0300

    fix queue deadlock

--- lua/queue.lua
+++ lua/queue.lua
@@ -3,7 +3,6 @@ local Queue = {}
 function Queue:new()
 	local instance = {
 		buffer = {},
-		is_locked = false,
 	}
 
 	setmetatable(instance, self)
@@ -17,42 +16,17 @@ function Queue:enqueue(text)
 		return
 	end
 
-	self:wait_until_unlocked()
-	self:_lock()
-
 	self.buffer = self.buffer or {}
-
 	table.insert(self.buffer, text)
-
-	self:_unlock()
 end
 
 function Queue:dequeue()
-	self:wait_until_unlocked()
-
-	self:_lock()
 	local result = nil
 	if self.buffer and self:is_filled() then
 		result = table.remove(self.buffer, 1)
 	end
-	self:_unlock()
 
 	return result
-end
-
-function Queue:_lock()
-	self.is_locked = true
-end
-
-function Queue:_unlock()
-	self.is_locked = false
-end
-
-function Queue:wait_until_unlocked()
-	local count = 0
-	while self.is_locked and count < 1000 do
-		count = count + 1
-	end
 end
 
 function Queue:is_filled()
